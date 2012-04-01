ad3a1b61e27253ea895ea6a78536b4883721cfdb
Rob Whitby <robwhitby78@yahoo.co.uk>
2012-04-01 20:24:38 +0100

    15 and 16

---
+++ 016.scala
@@ -0,0 +1,13 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+
+val sum = BigInt(2).pow(1000).toString.foldLeft(0)(_ + _.asDigit)
+
+println(sum)
